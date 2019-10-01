00289229e70df6bb2d5a3c5fef7e9c74112a6d7e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-01 08:51:22 +0200

    Creazione di un progetto in console

---
+++ SoluzioneMatematica/LibreriaMatematica/Operazioni.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LibreriaMatematica
+{
+    public class Operazioni
+    {
+        public static int Somma(int a, int b)
+        {
+            int somma = a + b;
+            return somma;
+        }
+        public static int Differenza(int a, int b)
+        {
+            int differenza = a - b;
+            return differenza;
+        }
+        public static int Prodotto(int a, int b)
+        {
+            int prodotto = a * b;
+            return prodotto;
+        }
+        public static double Quoziente(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new Exception("Divisore = 0");
+            }
+            double quoziente = a / b;
+            return quoziente;
+        }
+    }
+}
---
+++ SoluzioneMatematica/Matematica/Program.cs
@@ -0,0 +1,25 @@
+﻿using LibreriaMatematica;
+using System;
+
+namespace Matematica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il primo numero: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci il secondo numero: ");
+            int n2 = int.Parse(Console.ReadLine());
+            int somma = Operazioni.Somma(n1, n2);
+            int differenza = Operazioni.Differenza(n1, n2);
+            int prodotto = Operazioni.Prodotto(n1, n2);
+            double quoziente = Operazioni.Quoziente(n1, n2);
+            Console.WriteLine($"Somma = {somma}");
+            Console.WriteLine($"Differenza = {differenza}");
+            Console.WriteLine($"Prodotto = {prodotto}");
+            Console.WriteLine($"Quoziente = {quoziente}");
+            Console.ReadLine();
+        }
+    }
+}
